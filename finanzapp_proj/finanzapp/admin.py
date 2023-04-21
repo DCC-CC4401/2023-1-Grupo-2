caea85c9a272ff8ee258cc2265ee941e691d24c4
Lucas Murray <lucasmurrayh@gmail.com>
2023-04-21 09:34:17 -0400

    Created models: Users, Transactions, Categories

--- finanzapp_proj/finanzapp/admin.py
+++ finanzapp_proj/finanzapp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from finanzapp.models import  User, Transaction, Category
+
+admin.site.register(User)
+admin.site.register(Transaction)
+admin.site.register(Category)
--- finanzapp_proj/finanzapp/models.py
+++ finanzapp_proj/finanzapp/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils import timezone
 
-# Create your models here.
+# Usuarios
+class User(AbstractUser):
+    # Nombre que aparecerá en la página
+    display_name = models.CharField(max_length=100, blank=True)
+    # Monto, parte en 0
+    total = models.FloatField(default=0)
+    # Presupuesto, opcional
+    budget = models.FloatField(blank=True, null=True)
+
+# Transacciones
+class Transaction(models.Model):
+    # Tiene un único usuario asociado
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="transactions")
+    # Tipos de transacciones
+    types = [('spend', 'gasto'), ('deposit', 'abono')]
+    type = models.CharField(choices=types, max_length=100)
+    # Descripción de la transaccion
+    description = models.TextField(max_length=500)
+    # Monto de la transacción
+    amount = models.FloatField(default=0)
+    # Fecha de la transacción
+    date = models.DateField(default=timezone.now().strftime("%Y-%m-%d"))
+
+# Categorias 
+class Category(models.Model):
+    # Nombre de la categoria
+    name = models.CharField(max_length=100)
+    # Presupuesto asociado a la categoria, opcional
+    budget = models.FloatField(blank=True)
+    # Usuario que creo la categoría
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="categories")
