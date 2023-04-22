7509e2c59327c2d8fec9a233b6b9aa467fee1e28
Mansep_12 <manuel.sepulveda.c@ug.uchile.cl>
2023-04-22 11:12:19 -0400

    implementacion formulario inicio de sesion

--- finanzapp_proj/finanzapp/views.py
+++ finanzapp_proj/finanzapp/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import redirect, render
 
 # Create your views here.
 
-def inicio_sesion(request, usuario , clave):
 
-    return HttpResponse("Ingrese aquí:")
+def login1(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('logeado')  # Redirigir al usuario a la página admin
+        else:
+           return render(request, 'login.html', {'error_message': 'Nombre de usuario o contraseña incorrectos'})
+    else:
+        return render(request, 'login.html')
+
+def logeado(request):
+    return HttpResponse("logeado")
