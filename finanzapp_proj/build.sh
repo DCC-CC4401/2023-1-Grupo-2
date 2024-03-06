37eef5241fda8281b55ab776bdd5ab185c683c84
lucasmurray97 <37586981+lucasmurray97@users.noreply.github.com>
2024-03-06 13:11:17 -0300

    ready for deployment 5

--- finanzapp_proj/build.sh
+++ finanzapp_proj/build.sh
@@ -2,12 +2,12 @@
 # Exit on error
 set -o errexit
 
-cd finanzapp_proj
-ls
 
 # Modify this line as needed for your package manager (pip, poetry, etc.)
 pip install -r requirements.txt
 
+cd finanzapp_proj
+ls
 # Convert static asset files
 python manage.py collectstatic --no-input
 
